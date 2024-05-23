c584c1e9e249711c19ad6d049895604ec5a8bd97
wujia2022 <996705263@qq.com>
2024-05-23 13:18:57 +0800

    生成po类

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.CoursePublish;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程发布 服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2024-05-23
+ */
+public interface CoursePublishService extends IService<CoursePublish> {
+
+}
