9efe90d532ab28ecc64d2efdb5e3dbb733d6c946
wujia2022 <996705263@qq.com>
2024-05-24 09:34:54 +0800

    系统管理服务

---
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/edu/hunnu/xuecheng/system/config/GlobalCorsConfig.java
@@ -0,0 +1,39 @@
+package edu.hunnu.xuecheng.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * ClassName: GlobalCorsConfig
+ * Package: edu.hunnu.xuecheng.system.config
+ * Description: 跨域过虑器
+ *
+ * @Author: 吴佳
+ * @Create: 2024/5/23 - 20:37
+ * @Version: v1.0
+ */
+@Configuration
+public class GlobalCorsConfig {
+
+    /**
+     * 允许跨域调用的过滤器
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        //允许白名单域名进行跨域调用
+        config.addAllowedOrigin("*");
+        //允许跨越发送cookie
+        config.setAllowCredentials(true);
+        //放行全部原始头信息
+        config.addAllowedHeader("*");
+        //允许所有请求方法跨域调用
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ xuecheng-plus-system/xuecheng-plus-system-service/src/main/java/edu/hunnu/xuecheng/system/mapper/DictionaryMapper.java
@@ -0,0 +1,15 @@
+package edu.hunnu.xuecheng.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.hunnu.xuecheng.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
